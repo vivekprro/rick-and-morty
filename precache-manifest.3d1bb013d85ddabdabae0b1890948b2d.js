ef5e60b8c97fadc4ae563504a09bf759154bcbfa
VIVEK YADAV <VIVEKYADAV1412@GMAIL.COM>
2020-07-21 19:15:56 +0530

    Updates

---
+++ precache-manifest.3d1bb013d85ddabdabae0b1890948b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3d1ad5f407f1a566ce023c656e6dbb",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "8f602312f4e457ff219f",
+    "url": "/rick-and-morty/static/css/main.7798ee88.chunk.css"
+  },
+  {
+    "revision": "d3e60900822d15bcc4a7",
+    "url": "/rick-and-morty/static/js/2.4ef9c51c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rick-and-morty/static/js/2.4ef9c51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f602312f4e457ff219f",
+    "url": "/rick-and-morty/static/js/main.71f616b6.chunk.js"
+  },
+  {
+    "revision": "8ec831075791f93706ea",
+    "url": "/rick-and-morty/static/js/runtime-main.2b22dfe8.js"
+  }
+]);
